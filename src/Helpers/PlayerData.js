094a31df6a121b2b7787b9ca9be6ddb7d04a7e01
Nick Edwards <nickedwards.dev@gmail.com>
2024-12-05 14:11:02 -0800

    Image helper for players

---
+++ src/Helpers/PlayerData.js
@@ -0,0 +1,31 @@
+import adria_lamorticella from "../images/players/adria_lamorticella.jpeg";
+import anthony_van_winkle from "../images/players/anthony_van_winkle.png";
+// import claudine_mboligikpelani_nako from "../images/players/claudine_mboligikpelani_nako.jpg";
+import doug_willott from "../images/players/doug_willott.jpg";
+import ian_schempp from "../images/players/ian_schempp.jpg";
+import kat_schelonka from "../images/players/kat_schelonka.jpg";
+import mike_murphy from "../images/players/mike_murphy.jpg";
+import nathan_cox from "../images/players/nathan_cox.jpg";
+import nick_edwards from "../images/players/nick_edwards.jpg";
+import preston_mcneil from "../images/players/preston_mcneil.jpeg";
+import randy_miller from "../images/players/randy_miller.jpeg";
+// import rob_scherzer from "../images/players/rob_scherzer.jpg";
+import saina_hussain from "../images/players/saina_hussain.jpeg";
+import stephani_thompson from "../images/players/stephani_thompson.jpeg";
+
+export const images = [
+  { player: "Adria LaMorticella", img: adria_lamorticella },
+  { player: "Anthony Van Winkle", img: anthony_van_winkle },
+  // { player: "Claudine Mboligikpelani Nako", img: claudine_mboligikpelani_nako },
+  { player: "Doug Willott", img: doug_willott },
+  { player: "Ian Schempp", img: ian_schempp },
+  { player: "Kat Schelonka", img: kat_schelonka },
+  { player: "Mike Murphy", img: mike_murphy },
+  { player: "Nathan Cox", img: nathan_cox },
+  { player: "Nick Edwards", img: nick_edwards },
+  { player: "Preston McNeil", img: preston_mcneil },
+  { player: "Randy Miller", img: randy_miller },
+  // { player: "Rob Scherzer", img: rob_scherzer },
+  { player: "Saina Hussain", img: saina_hussain },
+  { player: "Stephani Thompson", img: stephani_thompson },
+];
